e5150210c4e553c1da68f348fe11d698181474ab
EverSeenTOTOTO <everseenflash@163.com>
2023-01-07 15:04:33 +0800

    update

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -1,66 +1,74 @@
 local autocmd = vim.api.nvim_create_autocmd
 
+vim.api.nvim_create_augroup("setfiletype", { clear = true })
+
 autocmd("BufRead,BufNewFile", {
-	pattern = "*.env.*",
-	callback = function()
-		vim.cmd("setfiletype sh")
-	end,
+  pattern = "*.env.*",
+  group = "setfiletype",
+  callback = function()
+    vim.cmd("setfiletype sh")
+  end,
 })
 
 autocmd("BufRead,BufNewFile", {
-	pattern = "*.s",
-	callback = function()
-		vim.cmd("setfiletype asm")
-	end,
+  pattern = "*.s",
+  group = "setfiletype",
+  callback = function()
+    vim.cmd("setfiletype asm")
+  end,
 })
 
 autocmd("BufRead,BufNewFile", {
-	pattern = "*.svelte",
-	callback = function()
-		vim.cmd("setfiletype html")
-	end,
+  pattern = "*.svelte",
+  group = "setfiletype",
+  callback = function()
+    vim.cmd("setfiletype html")
+  end,
 })
 
 autocmd("BufRead,BufNewFile", {
-	pattern = "*.wiki",
-	callback = function()
-		vim.cmd("setfiletype markdown")
-	end,
-})
-
--- Open a file from its last left off position
-autocmd("BufReadPost", {
-	pattern = "*.*",
-	callback = function()
-		if
-			not vim.fn.expand("%:p"):match(".git")
-			and vim.fn.line("'\"") > 1
-			and vim.fn.line("'\"") <= vim.fn.line("$")
-		then
-			vim.cmd("normal! g'\"")
-			vim.cmd("normal zz")
-		end
-	end,
+  pattern = "*.wiki",
+  group = "setfiletype",
+  callback = function()
+    vim.cmd("setfiletype markdown")
+  end,
 })
 
 -- File extension specific tabbing
 autocmd("Filetype", {
-	pattern = "python",
-	callback = function()
-		vim.opt_local.expandtab = true
-		vim.opt_local.tabstop = 4
-		vim.opt_local.shiftwidth = 4
-		vim.opt_local.softtabstop = 4
-	end,
+  pattern = "python",
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+  end,
 })
 
 autocmd("BufWritePre", {
-	pattern = "*.tsx,*.ts,*.jsx,*.js,*.vue",
-	callback = function()
-		for _, client in pairs(vim.lsp.buf_get_clients(0)) do
-			if client.name == "eslint" then
-				vim.cmd(":EslintFixAll")
-			end
-		end
-	end,
+  pattern = "*.tsx,*.ts,*.jsx,*.js,*.vue",
+  callback = function()
+    for _, client in pairs(vim.lsp.buf_get_clients(0)) do
+      if client.name == "eslint" then
+        vim.cmd(":EslintFixAll")
+      end
+    end
+  end,
+})
+
+-- automatically create directories
+autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(ctx)
+    local dir = vim.fn.fnamemodify(ctx.file, ":p:h")
+    vim.fn.mkdir(dir, "p")
+  end
+})
+
+-- highlight yanked text for 700ms
+autocmd("TextYankPost", {
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank { higroup = "IncSearch", timeout = 700 }
+  end
 })
--- boot.js
+++ boot.js
@@ -6,12 +6,12 @@ within(async () => {
   await $`echo ---------------------------------------------------- >> ${LogFile}`;
   await $`date >> ${LogFile}`;
 
-  await $`pm2 start -n dler ".config/clash/clash-linux-386" -- -f .config/clash/config.yml`;
+  // await $`pm2 start -n dler ".config/clash/clash-linux-386" -- -f .config/clash/config.yml`;
+  await $`pm2 start -n hutao ".config/clash/clash-linux-386" -- -f .config/clash/hutao.yml`;
   await $`pm2 start -n doc "pen" -- -p4000 -r repos/docs`;
   
   await $`pm2 start -n fcitx5 "fcitx5"`;
   await $`pm2 start -n hidden "v2ray" -- run --config .config/v2ray/config-hidden.json`;
-  await $`pm2 start -n v2ray "v2ray" -- run --config .config/v2ray/config.json`;
 
   // await cd('repos/tusk');
   // await $`pm2 start -n tusk yarn -- start`;
